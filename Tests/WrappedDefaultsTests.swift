51e88d222af642b084c9527269cebcf59ae6ca16
Jesse Squires <jesse@jessesquires.com>
2021-01-26 23:08:14 -0800

    headers + template

--- Tests/WrappedDefaultsTests.swift
+++ Tests/WrappedDefaultsTests.swift
@@ -1,6 +1,15 @@
 //
-// Created by Jesse Squires on 1/25/21.
-// 
+//  Created by Jesse Squires
+//  https://www.jessesquires.com
+//
+//  Documentation
+//  https://jessesquires.github.io/WrappedDefaults
+//
+//  GitHub
+//  https://github.com/jessesquires/WrappedDefaults
+//
+//  Copyright © 2021-present Jesse Squires
+//
 
 import XCTest
 @testable import WrappedDefaults
