a3e8c6500b5beca7fdaf49e96f8e8f13947ab829
Jesse Squires <jesse@jessesquires.com>
2021-01-26 23:28:51 -0800

    add swiftlint and docs scripts

---
+++ scripts/build_docs.zsh
@@ -0,0 +1,53 @@
+#!/bin/zsh
+
+#  Created by Jesse Squires
+#  https://www.jessesquires.com
+#
+#  Copyright © 2020-present Jesse Squires
+#
+#  Docs by jazzy
+#  https://github.com/realm/jazzy/releases/latest
+#  ------------------------------
+#  Generates documentation using jazzy and checks for installation.
+
+VERSION="0.13.6"
+
+FOUND=$(jazzy --version)
+LINK="https://github.com/realm/jazzy"
+INSTALL="gem install jazzy"
+
+PROJECT="WrappedDefaults"
+
+if which jazzy >/dev/null; then
+    jazzy \
+        --clean \
+        --author "Jesse Squires" \
+        --author_url "https://jessesquires.com" \
+        --github_url "https://github.com/jessesquires/$PROJECT" \
+        --module "$PROJECT" \
+        --source-directory . \
+        --readme "README.md" \
+        --documentation "Guides/*.md" \
+        --output docs/
+else
+    echo "
+    Error: Jazzy not installed!
+
+    Download: $LINK
+    Install: $INSTALL
+    "
+    exit 1
+fi
+
+if [ "$FOUND" != "jazzy version: $VERSION" ]; then
+    echo "
+    Warning: incorrect Jazzy installed! Please upgrade.
+    Expected: $VERSION
+    Found: $FOUND
+
+    Download: $LINK
+    Install: $INSTALL
+    "
+fi
+
+exit
---
+++ scripts/lint.zsh
@@ -0,0 +1,41 @@
+#!/bin/zsh
+
+#  Created by Jesse Squires
+#  https://www.jessesquires.com
+#
+#  Copyright © 2020-present Jesse Squires
+#
+#  SwiftLint
+#  https://github.com/realm/SwiftLint/releases/latest
+#  ------------------------------
+#  Runs SwiftLint and checks for installation.
+
+VERSION="0.42.0"
+
+FOUND=$(swiftlint version)
+LINK="https://github.com/realm/SwiftLint"
+INSTALL="brew install swiftlint"
+
+if which swiftlint >/dev/null; then
+    swiftlint lint --config ./.swiftlint.yml
+else
+    echo "
+    Error: SwiftLint not installed!
+
+    Download: $LINK
+    Install: $INSTALL
+    "
+fi
+
+if [ $FOUND != $VERSION ]; then
+    echo "
+    Warning: incorrect SwiftLint installed! Please upgrade.
+    Expected: $VERSION
+    Found: $FOUND
+
+    Download: $LINK
+    Install: $INSTALL
+    "
+fi
+
+exit
