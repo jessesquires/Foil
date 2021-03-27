59a21a2f30daf4dc36af07d8ac55941617640e74
Jesse Squires <jesse@jessesquires.com>
2021-03-26 22:40:24 -0700

    don't retain default value in wrapper

--- Sources/WrappedDefault.swift
+++ Sources/WrappedDefault.swift
@@ -14,10 +14,9 @@
 import Foundation
 
 /// A property wrapper that uses `UserDefaults` as a backing store,
-/// whose `wrappedValue` is non-optional and provides a **non-optional default value**.
+/// whose `wrappedValue` is non-optional and registers a **non-optional default value**.
 @propertyWrapper
 public struct WrappedDefault<T: UserDefaultsSerializable> {
-    private let _defaultValue: T
     private let _userDefaults: UserDefaults
 
     /// The key for the value in `UserDefaults`.
@@ -36,13 +35,12 @@ public struct WrappedDefault<T: UserDefaultsSerializable> {
     /// Initializes the property wrapper.
     /// - Parameters:
     ///   - keyName: The key for the value in `UserDefaults`.
-    ///   - defaultValue: The default value for the specified key.
+    ///   - defaultValue: The default value to register for the specified key.
     ///   - userDefaults: The `UserDefaults` backing store. The default value is `.standard`.
     public init(keyName: String,
                 defaultValue: T,
                 userDefaults: UserDefaults = .standard) {
         self.key = keyName
-        self._defaultValue = defaultValue
         self._userDefaults = userDefaults
         userDefaults.registerDefault(value: defaultValue, key: keyName)
     }
