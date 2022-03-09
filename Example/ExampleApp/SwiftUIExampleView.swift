7a75f00e4512de56eea51e1602147770073d626d
Basem Emara <basem@zamzam.io>
2022-03-09 01:29:12 -0500

    Add SwiftUI example

---
+++ Example/ExampleApp/SwiftUIExampleView.swift
@@ -0,0 +1,47 @@
+//
+//  Created by Jesse Squires
+//  https://www.jessesquires.com
+//
+//  Documentation
+//  https://jessesquires.github.io/Foil
+//
+//  GitHub
+//  https://github.com/jessesquires/Foil
+//
+//  Copyright © 2021-present Jesse Squires
+//
+
+import SwiftUI
+
+struct SwiftUIExampleView: View {
+    @State private var flagEnabled = false
+
+    var body: some View {
+        VStack {
+            Toggle(isOn: $flagEnabled) {
+                Text(flagEnabled.description)
+            }
+            GroupBox {
+                Button("Simulate outside trigger") {
+                    AppSettings.shared.flagEnabled.toggle()
+                }
+            }
+        }
+        .padding()
+        .navigationTitle("SwiftUI Example")
+        .onChange(of: flagEnabled) {
+            // User interaction stores into settings
+            AppSettings.shared.flagEnabled = $0
+        }
+        .onReceive(AppSettings.shared.$flagEnabled) {
+            // Listen to settings change
+            flagEnabled = $0
+        }
+    }
+}
+
+final class SwiftUIExampleHostViewController: UIHostingController<SwiftUIExampleView>{
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder, rootView: SwiftUIExampleView())
+    }
+}
